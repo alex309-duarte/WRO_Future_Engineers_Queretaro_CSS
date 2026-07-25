bef967fd3c4dd4b370a0c8dd5be9193489120613
alex309-duarte <57850217+alex309-duarte@users.noreply.github.com>
2026-07-25 10:21:34 -0600

    clean up repository
    build for windows, does not apply for linux

--- software/cpp/object_detection/build/x86_64/_deps/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maker/hailo-apps/hailo_apps/cpp/external/yaml-cpp
-# Build directory: /home/maker/hailo-apps/hailo_apps/cpp/object_detection/build/x86_64/_deps/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
