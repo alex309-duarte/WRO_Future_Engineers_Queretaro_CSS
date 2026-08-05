46339b3589ab8c148eaa894cfc66522987cbc822
alex309-duarte <manuel_alex_11@hotmail.com>
2026-08-05 17:36:37 -0600

    Software hailo10 raspberry
    add build folder

---
+++ software_h10/cpp/object_detection_original_h10/build/h10_original/CMakeFiles/object_detection.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/object_detection_original_h10/Oradar_S2L.cpp" "CMakeFiles/object_detection.dir/Oradar_S2L.cpp.o" "gcc" "CMakeFiles/object_detection.dir/Oradar_S2L.cpp.o.d"
+  "/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/hailo_infer.cpp" "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/hailo_infer.cpp.o" "gcc" "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/hailo_infer.cpp.o.d"
+  "/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/resources_manager.cpp" "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/resources_manager.cpp.o" "gcc" "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/resources_manager.cpp.o.d"
+  "/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/toolbox.cpp" "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/toolbox.cpp.o" "gcc" "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/toolbox.cpp.o.d"
+  "/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/object_detection_original_h10/object_detection.cpp" "CMakeFiles/object_detection.dir/object_detection.cpp.o" "gcc" "CMakeFiles/object_detection.dir/object_detection.cpp.o.d"
+  "/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/object_detection_original_h10/rasp_gpio.cpp" "CMakeFiles/object_detection.dir/rasp_gpio.cpp.o" "gcc" "CMakeFiles/object_detection.dir/rasp_gpio.cpp.o.d"
+  "/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/object_detection_original_h10/spike.cpp" "CMakeFiles/object_detection.dir/spike.cpp.o" "gcc" "CMakeFiles/object_detection.dir/spike.cpp.o.d"
+  "/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/object_detection_original_h10/utils/utils.cpp" "CMakeFiles/object_detection.dir/utils/utils.cpp.o" "gcc" "CMakeFiles/object_detection.dir/utils/utils.cpp.o.d"
+  "" "object_detection" "gcc" "CMakeFiles/object_detection.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ software_h10/cpp/object_detection_original_h10/build/h10_original/CMakeFiles/object_detection.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/object_detection.dir/link.d"
+  "CMakeFiles/object_detection.dir/Oradar_S2L.cpp.o"
+  "CMakeFiles/object_detection.dir/Oradar_S2L.cpp.o.d"
+  "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/hailo_infer.cpp.o"
+  "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/hailo_infer.cpp.o.d"
+  "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/resources_manager.cpp.o"
+  "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/resources_manager.cpp.o.d"
+  "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/toolbox.cpp.o"
+  "CMakeFiles/object_detection.dir/home/maker/WRO_Future_Engineers_Queretaro_CSS/software_h10/cpp/common/toolbox.cpp.o.d"
+  "CMakeFiles/object_detection.dir/object_detection.cpp.o"
+  "CMakeFiles/object_detection.dir/object_detection.cpp.o.d"
+  "CMakeFiles/object_detection.dir/rasp_gpio.cpp.o"
+  "CMakeFiles/object_detection.dir/rasp_gpio.cpp.o.d"
+  "CMakeFiles/object_detection.dir/spike.cpp.o"
+  "CMakeFiles/object_detection.dir/spike.cpp.o.d"
+  "CMakeFiles/object_detection.dir/utils/utils.cpp.o"
+  "CMakeFiles/object_detection.dir/utils/utils.cpp.o.d"
+  "object_detection"
+  "object_detection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/object_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
